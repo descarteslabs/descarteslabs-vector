48e5c88b694b7a9a11a33d3329c354fccbfbc458
Josh Garcia <126618887+josh-garcia-dl@users.noreply.github.com>
2023-08-08 16:21:35 -0600

    [DA-778] update client default api hostname (#59)
    GitOrigin-RevId: 319d0177e8912b90933e52ba60cfc43f721817eb

--- descarteslabs/vector/common.py
+++ descarteslabs/vector/common.py
@@ -2,9 +2,7 @@ import os
 
 import descarteslabs as dl
 
-API_HOST = os.getenv(
-    "VECTOR_API_HOST", "https://vector.appsci-production.aws.descarteslabs.com"
-)
+API_HOST = os.getenv("VECTOR_API_HOST", "https://vector.descarteslabs.com")
 
 
 def get_token():
